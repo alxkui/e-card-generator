e4922230195fb82868a49b17cb9fc9171b5f7789
Alex Kovacs <kovacs-a@ulster.ac.uk>
2020-05-14 23:11:00 +0100

    Updates

---
+++ precache-manifest.2a971b31b08acae85db97e37dafa72c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cd1fb819b6c26e7781d3a1311740dd",
+    "url": "/e-card-generator/index.html"
+  },
+  {
+    "revision": "4d8dd41d4527be249a5b",
+    "url": "/e-card-generator/static/css/main.06066f4a.chunk.css"
+  },
+  {
+    "revision": "c7a7fd6601ce44afebfb",
+    "url": "/e-card-generator/static/js/2.258aceaf.chunk.js"
+  },
+  {
+    "revision": "e97871073f2afea5deedbe6179b031ea",
+    "url": "/e-card-generator/static/js/2.258aceaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8dd41d4527be249a5b",
+    "url": "/e-card-generator/static/js/main.19ed7393.chunk.js"
+  },
+  {
+    "revision": "0dec477a62271eb3a7b6",
+    "url": "/e-card-generator/static/js/runtime-main.3bc2027f.js"
+  }
+]);
